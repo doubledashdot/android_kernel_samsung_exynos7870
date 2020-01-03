0217e51986c5c74b7f6bed37d8821e38e1293a9d
Sahitya Tummala <stummala@codeaurora.org>
2020-01-03 08:49:28 +0530

    f2fs: show the CP_PAUSE reason in checkpoint traces
    Remove the duplicate CP_UMOUNT enum and add the new CP_PAUSE
    enum to show the checkpoint reason in the trace prints.
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: Prashant-1695 <prashant33968@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- include/trace/events/f2fs.h
+++ include/trace/events/f2fs.h
@@ -9,6 +9,8 @@
 #define show_dev(dev)		MAJOR(dev), MINOR(dev)
 #define show_dev_ino(entry)	show_dev(entry->dev), (unsigned long)entry->ino
 
+TRACE_DEFINE_ENUM(CP_PAUSE);
+
 #define show_block_type(type)						\
 	__print_symbolic(type,						\
 		{ NODE,		"NODE" },				\
@@ -84,11 +86,12 @@
 
 #define show_cpreason(type)						\
 	__print_symbolic(type,						\
-		{ CP_UMOUNT,	"Umount" },				\
+		{ CP_UMOUNT,    "Umount" },                             \
 		{ CP_FASTBOOT,	"Fastboot" },				\
 		{ CP_SYNC,	"Sync" },				\
 		{ CP_RECOVERY,	"Recovery" },				\
 		{ CP_DISCARD,	"Discard" },				\
+		{ CP_PAUSE,	"Pause" },				\
 		{ CP_UMOUNT | CP_TRIMMED,	"Umount,Trimmed" })
 
 #define show_fsync_cpreason(type)					\
