15b9f6f1f1c2d9cee02773951c5b97d41896f38b
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-01-23 09:08:52 -0800

    af_unix: Annotate data-race of gc_in_progress in wait_for_unix_gc().
    [ Upstream commit 31e03207119a535d0b0e3b3a7f91983aeb2cb14d ]
    
    gc_in_progress is changed under spin_lock(&unix_gc_lock),
    but wait_for_unix_gc() reads it locklessly.
    
    Let's use READ_ONCE().
    
    Fixes: 5f23b734963e ("net: Fix soft lockups/OOM issues w/ unix garbage collector")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Link: https://lore.kernel.org/r/20240123170856.41348-2-kuniyu@amazon.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 0c41412330b8804ec9829ecb929a21d8cc2e06c5)
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- net/unix/garbage.c
+++ net/unix/garbage.c
@@ -278,7 +278,7 @@ void wait_for_unix_gc(void)
 	 */
 	if (unix_tot_inflight > UNIX_INFLIGHT_TRIGGER_GC && !gc_in_progress)
 		unix_gc();
-	wait_event(unix_gc_wait, gc_in_progress == false);
+	wait_event(unix_gc_wait, !READ_ONCE(gc_in_progress));
 }
 
 /* The external entry point: unix_gc() */
