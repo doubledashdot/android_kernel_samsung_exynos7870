68d4285d37ca8e29c448e23cacc3bcbbe3d4e7a7
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-18 13:14:30 +0800

    KernelSU: scope minimized manual hooks 2.2
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1618,6 +1618,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd,
+			struct filename **filename_ptr,
+			void *argv, void *envp, int *flags);
+#endif
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1631,6 +1637,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
