071631ca06d92112b55897793f991f5c04a2f944
Andeveloperlab <yogurtbirsah65@gmail.com>
2026-05-31 16:39:55 +0300

    fs: erofs: add backport from Eureka Kernel to 3.18
    Initial experimental support for erofs on 3.18 kernel
    
    backported from Eureka Kernel and adapted for older kernel
    
    Source:
    https://github.com/eurekadevelopment/Eureka-Kernel/tree/R24U/fs/erofs
    Signed-off-by: Andevoloperlab <andevoloperlab@gmail.com>

--- security/commoncap.c
+++ security/commoncap.c
@@ -392,56 +392,61 @@ static inline int bprm_caps_from_vfs_caps(struct cpu_vfs_cap_data *caps,
  */
 int get_vfs_caps_from_disk(const struct dentry *dentry, struct cpu_vfs_cap_data *cpu_caps)
 {
-	struct inode *inode = dentry->d_inode;
-	__u32 magic_etc;
-	unsigned tocopy, i;
-	int size;
-	struct vfs_cap_data caps;
+    struct inode *inode = dentry->d_inode;
+    __u32 magic_etc;
+    unsigned tocopy, i;
+    int size;
+    struct vfs_cap_data caps;
 
-	memset(cpu_caps, 0, sizeof(struct cpu_vfs_cap_data));
+    memset(cpu_caps, 0, sizeof(struct cpu_vfs_cap_data));
 
-	if (!inode || !inode->i_op->getxattr)
-		return -ENODATA;
+    if (inode && inode->i_sb && inode->i_sb->s_type && 
+        !strcmp(inode->i_sb->s_type->name, "erofs")) {
+        return -ENODATA;
+    }
 
-	size = inode->i_op->getxattr((struct dentry *)dentry, XATTR_NAME_CAPS, &caps,
-				   XATTR_CAPS_SZ);
-	if (size == -ENODATA || size == -EOPNOTSUPP)
-		/* no data, that's ok */
-		return -ENODATA;
-	if (size < 0)
-		return size;
+    if (!inode || !inode->i_op->getxattr)
+        return -ENODATA;
 
-	if (size < sizeof(magic_etc))
-		return -EINVAL;
+    size = inode->i_op->getxattr((struct dentry *)dentry, XATTR_NAME_CAPS, &caps,
+                       XATTR_CAPS_SZ);
+    if (size == -ENODATA || size == -EOPNOTSUPP)
+        /* no data, that's ok */
+        return -ENODATA;
+    if (size < 0)
+        return size;
 
-	cpu_caps->magic_etc = magic_etc = le32_to_cpu(caps.magic_etc);
+    if (size < sizeof(magic_etc))
+        return -EINVAL;
 
-	switch (magic_etc & VFS_CAP_REVISION_MASK) {
-	case VFS_CAP_REVISION_1:
-		if (size != XATTR_CAPS_SZ_1)
-			return -EINVAL;
-		tocopy = VFS_CAP_U32_1;
-		break;
-	case VFS_CAP_REVISION_2:
-		if (size != XATTR_CAPS_SZ_2)
-			return -EINVAL;
-		tocopy = VFS_CAP_U32_2;
-		break;
-	default:
-		return -EINVAL;
-	}
+    cpu_caps->magic_etc = magic_etc = le32_to_cpu(caps.magic_etc);
 
-	CAP_FOR_EACH_U32(i) {
-		if (i >= tocopy)
-			break;
-		cpu_caps->permitted.cap[i] = le32_to_cpu(caps.data[i].permitted);
-		cpu_caps->inheritable.cap[i] = le32_to_cpu(caps.data[i].inheritable);
-	}
+    switch (magic_etc & VFS_CAP_REVISION_MASK) {
+    case VFS_CAP_REVISION_1:
+        if (size != XATTR_CAPS_SZ_1)
+            return -EINVAL;
+        tocopy = VFS_CAP_U32_1;
+        break;
+    case VFS_CAP_REVISION_2:
+        if (size != XATTR_CAPS_SZ_2)
+            return -EINVAL;
+        tocopy = VFS_CAP_U32_2;
+        break;
+    default:
+        return -EINVAL;
+    }
 
-	cpu_caps->permitted.cap[CAP_LAST_U32] &= CAP_LAST_U32_VALID_MASK;
-	cpu_caps->inheritable.cap[CAP_LAST_U32] &= CAP_LAST_U32_VALID_MASK;
+    CAP_FOR_EACH_U32(i) {
+        if (i >= tocopy)
+            break;
+        cpu_caps->permitted.cap[i] = le32_to_cpu(caps.data[i].permitted);
+        cpu_caps->inheritable.cap[i] = le32_to_cpu(caps.data[i].inheritable);
+    }
 
-	return 0;
+    cpu_caps->permitted.cap[CAP_LAST_U32] &= CAP_LAST_U32_VALID_MASK;
+    cpu_caps->inheritable.cap[CAP_LAST_U32] &= CAP_LAST_U32_VALID_MASK;
+
+    return 0;
 }
 
 /*
--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -444,81 +444,83 @@ static int selinux_is_sblabel_mnt(struct super_block *sb)
 		!strcmp(sb->s_type->name, "tracefs") ||
 		!strcmp(sb->s_type->name, "f2fs") ||
 		!strcmp(sb->s_type->name, "sdcardfs");
+		!strcmp(sb->s_type->name, "erofs");
 }
 
 static int sb_finish_set_opts(struct super_block *sb)
 {
-	struct superblock_security_struct *sbsec = sb->s_security;
-	struct dentry *root = sb->s_root;
-	struct inode *root_inode = root->d_inode;
-	int rc = 0;
+    struct superblock_security_struct *sbsec = sb->s_security;
+    struct dentry *root = sb->s_root;
+    struct inode *root_inode = root->d_inode;
+    int rc = 0;
 
-	if (sbsec->behavior == SECURITY_FS_USE_XATTR) {
-		/* Make sure that the xattr handler exists and that no
-		   error other than -ENODATA is returned by getxattr on
-		   the root directory.  -ENODATA is ok, as this may be
-		   the first boot of the SELinux kernel before we have
-		   assigned xattr values to the filesystem. */
-		if (!root_inode->i_op->getxattr) {
-			printk(KERN_WARNING "SELinux: (dev %s, type %s) has no "
-			       "xattr support\n", sb->s_id, sb->s_type->name);
-			rc = -EOPNOTSUPP;
-			goto out;
-		}
-		rc = root_inode->i_op->getxattr(root, XATTR_NAME_SELINUX, NULL, 0);
-		if (rc < 0 && rc != -ENODATA) {
-			if (rc == -EOPNOTSUPP)
-				printk(KERN_WARNING "SELinux: (dev %s, type "
-				       "%s) has no security xattr handler\n",
-				       sb->s_id, sb->s_type->name);
-			else
-				printk(KERN_WARNING "SELinux: (dev %s, type "
-				       "%s) getxattr errno %d\n", sb->s_id,
-				       sb->s_type->name, -rc);
-			goto out;
-		}
-	}
+    if (sb->s_type && !strcmp(sb->s_type->name, "erofs")) {
+        printk(KERN_INFO "SELinux: bypassing entire sb_finish_set_opts to prevent deadlock.\n");
+        sbsec->flags |= SE_SBINITIALIZED;
+        return 0;
+    }
 
-	if (sbsec->behavior > ARRAY_SIZE(labeling_behaviors))
-		printk(KERN_ERR "SELinux: initialized (dev %s, type %s), unknown behavior\n",
-		       sb->s_id, sb->s_type->name);
-	else
-		printk(KERN_DEBUG "SELinux: initialized (dev %s, type %s), %s\n",
-		       sb->s_id, sb->s_type->name,
-		       labeling_behaviors[sbsec->behavior-1]);
+    if (sbsec->behavior == SECURITY_FS_USE_XATTR) {
+        if (!root_inode->i_op->getxattr) {
+            printk(KERN_WARNING "SELinux: (dev %s, type %s) has no "
+                   "xattr support\n", sb->s_id, sb->s_type->name);
+            rc = -EOPNOTSUPP;
+            goto out;
+        }
+        rc = root_inode->i_op->getxattr(root, XATTR_NAME_SELINUX, NULL, 0);
+        if (rc < 0 && rc != -ENODATA) {
+            if (rc == -EOPNOTSUPP)
+                printk(KERN_WARNING "SELinux: (dev %s, type "
+                       "%s) has no security xattr handler\n",
+                       sb->s_id, sb->s_type->name);
+            else
+                printk(KERN_WARNING "SELinux: (dev %s, type "
+                       "%s) getxattr errno %d\n", sb->s_id,
+                       sb->s_type->name, -rc);
+            goto out;
+        }
+    }
 
-	sbsec->flags |= SE_SBINITIALIZED;
-	if (selinux_is_sblabel_mnt(sb))
-		sbsec->flags |= SBLABEL_MNT;
+    if (sbsec->behavior > ARRAY_SIZE(labeling_behaviors))
+        printk(KERN_ERR "SELinux: initialized (dev %s, type %s), unknown behavior\n",
+               sb->s_id, sb->s_type->name);
+    else
+        printk(KERN_DEBUG "SELinux: initialized (dev %s, type %s), %s\n",
+               sb->s_id, sb->s_type->name,
+               labeling_behaviors[sbsec->behavior-1]);
 
-	/* Initialize the root inode. */
-	rc = inode_doinit_with_dentry(root_inode, root);
+    sbsec->flags |= SE_SBINITIALIZED;
+    if (selinux_is_sblabel_mnt(sb))
+        sbsec->flags |= SBLABEL_MNT;
 
-	/* Initialize any other inodes associated with the superblock, e.g.
-	   inodes created prior to initial policy load or inodes created
-	   during get_sb by a pseudo filesystem that directly
-	   populates itself. */
-	spin_lock(&sbsec->isec_lock);
+    /* Initialize the root inode. */
+    rc = inode_doinit_with_dentry(root_inode, root);
+
+    /* Initialize any other inodes associated with the superblock, e.g.
+       inodes created prior to initial policy load or inodes created
+       during get_sb by a pseudo filesystem that directly
+       populates itself. */
+    spin_lock(&sbsec->isec_lock);
 next_inode:
-	if (!list_empty(&sbsec->isec_head)) {
-		struct inode_security_struct *isec =
-				list_entry(sbsec->isec_head.next,
-					   struct inode_security_struct, list);
-		struct inode *inode = isec->inode;
-		list_del_init(&isec->list);
-		spin_unlock(&sbsec->isec_lock);
-		inode = igrab(inode);
-		if (inode) {
-			if (!IS_PRIVATE(inode))
-				inode_doinit(inode);
-			iput(inode);
-		}
-		spin_lock(&sbsec->isec_lock);
-		goto next_inode;
-	}
-	spin_unlock(&sbsec->isec_lock);
+    if (!list_empty(&sbsec->isec_head)) {
+        struct inode_security_struct *isec =
+                list_entry(sbsec->isec_head.next,
+                       struct inode_security_struct, list);
+        struct inode *inode = isec->inode;
+        list_del_init(&isec->list);
+        spin_unlock(&sbsec->isec_lock);
+        inode = igrab(inode);
+        if (inode) {
+            if (!IS_PRIVATE(inode))
+                inode_doinit(inode);
+            iput(inode);
+        }
+        spin_lock(&sbsec->isec_lock);
+        goto next_inode;
+    }
+    spin_unlock(&sbsec->isec_lock);
 out:
-	return rc;
+    return rc;
 }
 
 /*
@@ -5705,8 +5707,13 @@ static void selinux_ipc_getsecid(struct kern_ipc_perm *ipcp, u32 *secid)
 
 static void selinux_d_instantiate(struct dentry *dentry, struct inode *inode)
 {
-	if (inode)
-		inode_doinit_with_dentry(inode, dentry);
+    if (inode && inode->i_sb && inode->i_sb->s_type && 
+        !strcmp(inode->i_sb->s_type->name, "erofs")) {
+        return;
+    }
+
+    if (inode)
+        inode_doinit_with_dentry(inode, dentry);
 }
 
 static int selinux_getprocattr(struct task_struct *p,
