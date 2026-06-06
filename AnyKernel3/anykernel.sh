cfadbb741b6142d9a2d00e48fd40034b5428660c
//. <174224977+skye-tachyon@users.noreply.github.com>
2026-06-06 15:05:23 -0400

    AnyKernel3: Import
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,42 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=J7 Neo/NxT sKernelSU - //.
+
+do.devicecheck=0
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+'; } # end properties
+
+# shell variables
+block=/dev/block/by-name/BOOT;
+is_slot_device=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+# end ramdisk changes
+
+write_boot;
+## end install
+
