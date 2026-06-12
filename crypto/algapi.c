27ed6f1c3fa0f4e235f824c76728971b3445e49a
//. <174224977+skye-tachyon@users.noreply.github.com>
2026-06-12 15:56:02 -0400

    crypto: disable fips 200 -eacces
    * causes kernelsu to fail manager sha256 verification
    
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- crypto/algapi.c
+++ crypto/algapi.c
@@ -43,14 +43,6 @@ static inline int crypto_set_driver_name(struct crypto_alg *alg)
 
 static int crypto_check_alg(struct crypto_alg *alg)
 {
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err())) {
-		printk(KERN_ERR
-			"crypto_check_alg failed due to FIPS error: %s",
-			alg->cra_name);
-		return -EACCES;
-	}
-#endif
 	
 	if (alg->cra_alignmask & (alg->cra_alignmask + 1))
 		return -EINVAL;
@@ -396,15 +388,6 @@ int crypto_register_alg(struct crypto_alg *alg)
 	struct crypto_larval *larval;
 	int err;
 
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err())) {
-		printk(KERN_ERR
-			"Unable to registrer alg: %s because of FIPS ERROR\n"
-			, alg->cra_name);
-		return -EACCES;
-	}
-#endif
-
 	err = crypto_check_alg(alg);
 	if (err)
 		return err;
@@ -496,11 +479,6 @@ int crypto_register_template(struct crypto_template *tmpl)
 	struct crypto_template *q;
 	int err = -EEXIST;
 
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err()))
-		return -EACCES;
-#endif
-
 	down_write(&crypto_alg_sem);
 
 	//crypto_check_module_sig(tmpl->module);
@@ -570,13 +548,6 @@ static struct crypto_template *__crypto_lookup_template(const char *name)
 
 struct crypto_template *crypto_lookup_template(const char *name)
 {
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err())) {
-		printk(KERN_ERR
-			"crypto_lookup failed due to FIPS error: %s", name);
-		return ERR_PTR(-EACCES);
-	}
-#endif
 	return try_then_request_module(__crypto_lookup_template(name),
 				       "crypto-%s", name);
 }
@@ -587,11 +558,6 @@ int crypto_register_instance(struct crypto_template *tmpl,
 {
 	struct crypto_larval *larval;
 	int err;
-
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err()))
-		return -EACCES;
-#endif
 
 	err = crypto_check_alg(&inst->alg);
 	if (err)
@@ -657,11 +623,6 @@ int crypto_init_spawn(struct crypto_spawn *spawn, struct crypto_alg *alg,
 		      struct crypto_instance *inst, u32 mask)
 {
 	int err = -EAGAIN;
-
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err()))
-		return -EACCES;
-#endif
 
 	spawn->inst = inst;
 	spawn->mask = mask;
@@ -876,11 +837,6 @@ void *crypto_alloc_instance2(const char *name, struct crypto_alg *alg,
 	char *p;
 	int err;
 
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err()))
-		return ERR_PTR(-EACCES);
-#endif
-
 	p = kzalloc(head + sizeof(*inst) + sizeof(struct crypto_spawn),
 		    GFP_KERNEL);
 	if (!p)
@@ -911,11 +867,6 @@ struct crypto_instance *crypto_alloc_instance(const char *name,
 	struct crypto_instance *inst;
 	struct crypto_spawn *spawn;
 	int err;
-
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err()))
-		return ERR_PTR(-EACCES);
-#endif
 
 	inst = crypto_alloc_instance2(name, alg, 0);
 	if (IS_ERR(inst))
@@ -952,11 +903,6 @@ int crypto_enqueue_request(struct crypto_queue *queue,
 			   struct crypto_async_request *request)
 {
 	int err = -EINPROGRESS;
-
-#ifdef CONFIG_CRYPTO_FIPS
-	if (unlikely(in_fips_err()))
-		return -EACCES;
-#endif
 
 	if (unlikely(queue->qlen >= queue->max_qlen)) {
 		err = -EBUSY;
